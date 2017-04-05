73a83760bf9b9e3d3c618b0e37de1adfa2ae96c0
toussebastian <toussebastian13@gmail.com>
2017-04-04 20:38:12 -0500

    finish the datatypes part
    i am gonna to the second part that is conditionals

---
+++ DataType and Va.js
@@ -0,0 +1,93 @@
+// JavaScript Document
+
+/*
+ * Programming Quiz: First Expression (2-1)
+ */
+
+// this expression equals 4, change it to equal 42
+console.log(1 + 5 - 2);
+
+// my Answer
+
+console.log(10*10/2-8);
+
+/*
+ * Programming Quiz: Converting Tempatures (2-2)
+ */
+
+var celsius = 12;
+var fahrenheit = /* convert celsius to fahrenheit here */
+
+console.log(/* print out result here */);
+
+// my Answer 
+
+var celsius = 12;
+var fahrenheit = celsius * 1.8 + 32;
+
+console.log(fahrenheit);
+
+
+/*
+ * Programming Quiz: Favorite Food (2-3)
+ */
+var favoriteFood = "Ramen";
+console.log(favoriteFood);
+
+/*
+ * Programming Quiz: String Equality for All (2-4)
+ */
+
+// fix the right side of the expression
+var answer = "ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal";
+console.log(answer);
+
+// my Answer 
+
+var answer = "ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal";
+
+/*
+ * Programming Quiz: All Tied Up (2-5)
+ */
+
+var joke = "Why couldn't the shoes go out and play?\nThey were all \"tied\" up! "
+
+console.log(joke);
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+var haiku = "Blowing from the west\n"+"Fallen leaves gather\n"+"In the east.";
+console.log(haiku);
+
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+
+// your code goes here
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+
+// your code goes here
+var thingOne = "red";
+var thingTwo = "blue";
+console.log(thingOne + thingTwo);
+
+
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ */
+
+// your code goes here
+var fullName = "sebastina ";
+
+/*
+ * Programming Quiz: Out to Dinner (2-10)
+ */
+
+// your code goes here
+ var bill  = 10.25 + 3.99 + 7.15;
+ var tip = bill * 0.15;
+ var total  = bill + tip;
+ console.log(total);
